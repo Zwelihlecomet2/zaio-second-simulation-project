5de9e9be2ca267d5f0a306f238a2d2650a320b3c
zwelihle comet <zwelihlecomet2@gmail.com>
2025-03-22 22:02:19 +0000

    added the functionality from the third stand up

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ class App {
         this.$mainContainer = document.querySelector(".main-container");
         this.$firebaseuiAuthContainer = document.querySelector("#firebaseui-auth-container");
         this.$logoutButton = document.querySelector("#logout-button");
-
+        this.$uploadButton = document.querySelector("#uploadButton");
+        this.$overlay = document.querySelector("#overlay");
+        this.$uploadModal = document.querySelector("#uploadModal");
+        this.$closeModal = document.querySelector("#closeModal");
 
         // Initialize the FirebaseUI Widget using Firebase.
         this.ui = new firebaseui.auth.AuthUI(auth);
@@ -19,7 +22,29 @@ class App {
             this.$logoutButton.addEventListener("click", (event) =>{
                 this.handleLogout(event);
             });
+
+            // document.addEventListener("DOMContentLoaded", (event) =>{
+
+            // });
+
+             // Show the modal and overlay when the "Upload" button is clicked
+            uploadButton.addEventListener("click", (event) =>{
+                this.handleUploadModal(event);
+                console.log(event);
+            });
         }); 
+    }
+
+    handleUploadModal(){
+        if(this.$overlay.classList.contains("hidden") && this.$uploadModal.classList.contains("hidden")){
+            this.$overlay.classList.remove("hidden");
+            this.$uploadModal.classList.remove("hidden");
+        }
+
+        else{
+            this.$overlay.classList.add("hidden");
+            this.$uploadModal.classList.add("hidden"); 
+        }
     }
 
     handleAuth(){
